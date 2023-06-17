3beec029900f1c243ff7afb86b014cdb110f640f
MariuszKuich <mariuszkuich@outlook.com>
2023-06-17 12:34:45 +0200

    17.06 - Equal Sides Of An Array (improvements)

--- src/main/java/pl/mariuszk/kyu6/EqualSidesOfAnArray.java
+++ src/main/java/pl/mariuszk/kyu6/EqualSidesOfAnArray.java
@@ -6,16 +6,13 @@ public class EqualSidesOfAnArray {
 
     public static int findEvenIndex(int[] arr) {
         int sum = Arrays.stream(arr).sum();
-        if (sum == 0) {
-            return 0;
-        }
         int leftSideSum = 0;
         for (int idx = 0; idx < arr.length; idx++) {
             sum -= arr[idx];
-            leftSideSum += (idx > 0) ? arr[idx - 1] : 0;
             if (leftSideSum == sum) {
                 return idx;
             }
+            leftSideSum += arr[idx];
         }
         return -1;
     }
